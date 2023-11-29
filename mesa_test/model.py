dcc0fcb190187e9cbe5d47e7bd7e1c4c1cd46d83
Justin <justin.keil@vrg.de>
2023-11-29 11:06:17 +0100

    Lagerplatz und Ware werden im Grid mit erzeugt.
    Lagerplätze und Waren werden jetzt auch im Grid erzeugt und heben sich durch eine andere Farbe und Schriftzug hervor.

--- mesa_test/model.py
+++ mesa_test/model.py
@@ -2,30 +2,61 @@ from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
-from agent import TGabelstapler
+from agent import TWare, TLagerplatz, TGabelstapler
 
 class OurModel(Model):
-    def __init__(self, number_agents, width, height):
-        self.num_agents = number_agents
+    def __init__(self, number_agents_gabelstapler, number_agents_lagerplatz, number_agents_ware, width, height):
+        self.num_agents_gabelstapler = number_agents_gabelstapler
+        self.num_agents_lagerplatz = number_agents_lagerplatz
+        self.num_agents_ware = number_agents_ware
+        
         self.grid = MultiGrid(width, height, False)
         self.schedule = RandomActivation(self)
         self.running = True
 
         self.datacollector_currents = DataCollector(
             {
-                "Anzahl Gabelstapler": self.num_agents,
+                "Anzahl Gabelstapler": self.num_agents_gabelstapler,
+                "Anzahl der Lagerplätze": self.num_agents_lagerplatz,
+                "Anzahl der Waren": self.num_agents_ware,
             }
         )
 
         # Create agents
-        for i in range(self.num_agents):
-            a = TGabelstapler(i, self)
+        agent_index = 0
+        
+        # Create TWare
+        for i in range(agent_index, self.num_agents_ware + agent_index):
+            a = TWare(agent_index, self)
+            self.schedule.add(a)
+
+            # Add the agent to a random grid
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+            agent_index= agent_index + 1
+
+        #Create TLagerplatz
+        for i in range(agent_index, self.num_agents_lagerplatz + agent_index):
+            a = TLagerplatz(agent_index, self)
+            self.schedule.add(a)
+
+            # Add the agent to a random grid
+            x = 3 + i
+            y = 10
+            self.grid.place_agent(a, (x, y))
+            agent_index= agent_index + 1
+
+        #Create TGapelstapler
+        for i in range(agent_index, self.num_agents_gabelstapler + agent_index):
+            a = TGabelstapler(agent_index, self)
             self.schedule.add(a)
 
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
+            agent_index= agent_index + 1
 
     def step(self):
         """Advance the model by one step."""
--- mesa_test/server.py
+++ mesa_test/server.py
@@ -1,7 +1,8 @@
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import NumberInput
-from mesa.visualization.modules import CanvasGrid#, ChartModule
+from mesa.visualization.modules import CanvasGrid
 from model import OurModel
+from agent import TWare, TLagerplatz, TGabelstapler
 
 NUMBER_OF_CELLS = 15
 
@@ -9,8 +10,14 @@ SIZE_OF_CANVAS_IN_PIXELS_X = 1000
 SIZE_OF_CANVAS_IN_PIXELS_Y = 1000
 
 simulation_params = {
-    "number_agents": NumberInput(
+    "number_agents_gabelstapler": NumberInput(
         "Anzahl der Gabelstapler", value=5
+    ),
+    "number_agents_lagerplatz": NumberInput(
+    "Anzahl der Lagerplätze", value=5
+    ),
+    "number_agents_ware": NumberInput(
+    "Anzahl der Waren", value=5
     ),
     "width": NUMBER_OF_CELLS,
     "height": NUMBER_OF_CELLS,
@@ -20,17 +27,53 @@ simulation_params = {
 def agent_portrayal(agent):
     # if the agent is buried we put it as white, not showing it.
     #if agent.buried:
-    portrayal = {
-        "Shape": "rect",
-        "Filled": "true",
-        "Color": "yellow",
-        "w": "1",
-        "h": "1",
-        "text": "Gabelstapler",
-        "Layer": 1,
-        "text_color": "black",
-    }
+    if isinstance(agent, TGabelstapler):
+        portrayal = {
+            "Shape": "rect",
+            "Filled": "true",
+            "Color": "yellow",
+            "w": "1",
+            "h": "1",
+            "text": "Gabelstapler",
+            "Layer": 1,
+            "text_color": "black",
+        }
 
+    elif isinstance(agent, TWare):
+        portrayal = {
+            "Shape": "rect",
+            "Filled": "true",
+            "Color": "blue",
+            "w": "1",
+            "h": "1",
+            "text": "War",
+            "Layer": 1,
+            "text_color": "black",
+        }
+
+    elif isinstance(agent, TLagerplatz):
+        portrayal = {
+            "Shape": "rect",
+            "Filled": "true",
+            "Color": "gray",
+            "w": "1",
+            "h": "1",
+            "text": "Lagerplatz",
+            "Layer": 1,
+            "text_color": "black",
+        }
+
+    else:
+        portrayal = {
+           "Shape": "rect",
+           "Filled": "true",
+           "Color": "green",
+           "w": "1",
+           "h": "1",
+           "text": "Default",
+           "Layer": 1,
+           "text_color": "black",
+        }
     return portrayal
 
 
